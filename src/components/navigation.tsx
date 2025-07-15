93616346e7001789bd3d81551c9cb3cde4c630dd
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-15 05:59:17 +0300

    signin and signout

---
+++ src/components/navigation.tsx
@@ -0,0 +1,16 @@
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <section className="px-8 py-6 flex justify-between items-center border-b border-gray-300">
+      <div className="font-semibold text-xl">
+        <Link href="/">Clerk App</Link>
+      </div>
+      <div className="flex gap-4">
+        <SignInButton mode="modal" />
+        <SignOutButton />
+      </div>
+    </section>
+  );
+}
